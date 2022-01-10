4c073268e61474919cb8be90da1cf5d3d876b4e6
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-01-10 13:10:15 +0100

    Revert "power: remove dependency to vendor.lineage.power@1.0."
    This reverts commit b03d9642

--- hidl/power/Android.mk
+++ hidl/power/Android.mk
@@ -31,7 +31,8 @@ LOCAL_SHARED_LIBRARIES := \
     libbinder \
     libhidlbase \
     libutils \
-    android.hardware.power@1.0
+    android.hardware.power@1.0 \
+    vendor.lineage.power@1.0
 
 # LOCAL_STATIC_LIBRARIES := libc++fs
 
