82e07f3b118e2f0284a0d4a9cd0f5b339de9f018
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-09-30 14:17:19 +0200

    gralloc: re-enable ION allocation for HWC

--- exynos4/hal/libgralloc_ump/alloc_device.cpp
+++ exynos4/hal/libgralloc_ump/alloc_device.cpp
@@ -296,7 +296,7 @@ static int gralloc_alloc_buffer(alloc_device_t* dev, size_t size, int usage,
     }
 
     ret = -1;
-    if (usage & (GRALLOC_USAGE_HW_ION)) {
+    if (usage & (GRALLOC_USAGE_HW_ION | GRALLOC_USAGE_HW_COMPOSER)) {
         ALOGD("%s: Allocating graphicbuffer via ION (size: %d)...", __func__, size);
         priv_alloc_flag = priv_alloc_flag | private_handle_t::PRIV_FLAGS_GRAPHICBUFFER;
         ret = gralloc_alloc_ion(dev, size, usage, format, &ion_fd, &ion_paddr, &priv_alloc_flag, &ump_mem_handle);
